d16c68e368cdcc8bcfd6cac7d326e6c67bc341ac
ali-karbalaei-hossienie <mp.karbalaei@gmail.com>
2026-09-12 23:18:22 +0330

    refactor(config): streamline baseURL assignment and improve axios configuration

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  typeof window !== "undefined"
+    ? "/api"
+    : process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+
 const app = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL,
   withCredentials: true,
 });
 
